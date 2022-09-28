02da78cb14d6cda8a108d3ccec828918f094ca01
Letterbombs <92528742+Letterbombs@users.noreply.github.com>
2022-09-28 20:16:57 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package PBO;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        JOptionPane.showInternalMessageDialog(null, "Belajar Java\n sangat mudah sekali\ndan sangat menyenangkan");
+        JOptionPane.showInputDialog("Anda sedang belajar apa ?");
+        JOptionPane.showInternalMessageDialog(null, "Belajar pemrograman web sangat mudah");
+    }
+}
